a28ce6cb4da729bd60db656486b8fd279bb962c6
Le Goff Maël <legoffmael@gmail.com>
2017-02-22 18:40:59 +0100

    Ajout du Menu state codé

--- core/src/com/pts4/game/PTS4.java
+++ core/src/com/pts4/game/PTS4.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.pts4.game.states.GameStateManager;
+import com.pts4.game.states.MenuState;
 
 public class PTS4 extends ApplicationAdapter {
 	//La largeur de la fenêtre
@@ -24,11 +25,23 @@ public class PTS4 extends ApplicationAdapter {
 		batch = new SpriteBatch();
 
 		gsm = new GameStateManager();
+
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+
+		gsm.push(new MenuState(gsm));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		gsm.update(Gdx.graphics.getDeltaTime());
+
+		gsm.render(batch);
+	}
+
+	@Override
+	public void dispose() {
+		super.dispose();
 	}
 }
